7853131547bad2331e97efa23ba15a23c547ee00
Ankur Yadav <100476705+AnkurYadav1999@users.noreply.github.com>
2022-08-01 16:05:18 +0530

    Add files via upload

---
+++ mongodb.js
@@ -0,0 +1,13 @@
+const { MongoClient } = require('mongodb')
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+
+async function dbconnection() {
+
+    let result = await client.connect();
+    let db = result.db('ecomm');
+    return db.collection('products');
+}
+
+module.exports=dbconnection;
